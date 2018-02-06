06ab944a4f9f4ba5a382bf657032c309389de03a
Christopher Wu <wu.christophert@gmail.com>
2018-02-06 14:12:28 -0800

    missed src folder

---
+++ src/hello/sample_functions.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "fmt"
+)
+
+func calculateBill(price, no int) int {
+  var totalPrice  = price * no
+  return totalPrice
+}
+
+func main() {
+  price, no := 90, 6
+  totalPrice := calculateBill(price, no)
+  fmt.Println("Your total is", totalPrice)
+}
