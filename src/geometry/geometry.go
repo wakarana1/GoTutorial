28fd2bdbfe8027b53d6167c7ffe6ef2ce4628723
Christopher Wu <wu.christophert@gmail.com>
2018-02-12 16:42:00 -0800

    geometry.go check to see if len & wid > 0

--- src/geometry/geometry.go
+++ src/geometry/geometry.go
@@ -5,10 +5,26 @@ import (
   "geometry/rectangle" // IMPORTING CUSTOM PACKAGE
   "log"
 )
+/*
+ * 1. package variables
+*/
+ var rectLen, rectWidth float64 = 6, 7
+ /*
+ * 2. init function to check if length and width are greater than zero
+ */
+
+ func init() {
+   println("main package initialized")
+   if rectLen < 0 {
+    log.Fatal("length is less than zero")
+   }
+   if rectWidth < 0 {
+    log.Fatal("width is less than zero")
+   }
+ }
 
 func main() {
-  var rectLen, rectWidth float64 = 6, 7
-  fmt.Println("GEometrical shape properties")
+  fmt.Println("Geometrical shape properties")
     /* Area function of rectangle package used
     */
   fmt.Printf("area of rectangle %.2f\n", rectangle.Area(rectLen, rectWidth))
