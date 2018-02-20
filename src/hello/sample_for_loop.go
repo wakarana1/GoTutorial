d98bc54587da7fab5e8346c2fd551cb4fc435bb6
Christopher Wu <wu.christophert@gmail.com>
2018-02-19 17:27:08 -0800

    different for-loop statements

---
+++ src/hello/sample_for_loop.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  //SIMPLE FOR LOOP
+  // for i := 1; i <= 10; i++ {
+  //   fmt.Printf("%d ", i)
+  // }
+
+  // FOR LOOP WITH BREAK
+  // for i := 1; i <= 10; i++ {
+  //   if i > 5 {
+  //     break //loop is terminated if i > 5
+  //   }
+  //   fmt.Printf("%d ", i)
+  // }
+  // fmt.Printf("\nline after for loop")
+
+  // CONTINUE WILL SKIP THE REST OF THE FUNCTION
+  // for i := 1; i <= 10; i++ {
+  //   if i%2 == 0 {
+  //     continue
+  //   }
+  //   fmt.Printf("%d ", i)
+  // }
+
+}
